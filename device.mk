c3684e4dae136e670fa9cbe3f0e7141e0bd376ed
xunmod <116288337+xunmod@users.noreply.github.com>
2024-02-02 18:19:54 +0800

    Add module config; enable cam&gps

--- device.mk
+++ device.mk
@@ -23,16 +23,17 @@ PRODUCT_COPY_FILES += \
     device/ONDA/octopus-ibt/modules/modules/disp.ko:root/disp.ko \
     device/ONDA/octopus-ibt/modules/modules/sw-device.ko:obj/sw-device.ko \
     device/ONDA/octopus-ibt/modules/modules/gt9xxf_ts.ko:obj/gt9xxf_ts.ko \
+    device/ONDA/octopus-ibt/modules/modules/gslX680new.ko:obj/gslX680new.ko
 
 
 PRODUCT_COPY_FILES += \
     device/ONDA/octopus-ibt/configs/camera.cfg:system/etc/camera.cfg \
     device/ONDA/octopus-ibt/configs/cfg-Gallery2.xml:system/etc/cfg-Gallery2.xml \
     device/ONDA/octopus-ibt/configs/media_profiles.xml:system/etc/media_profiles.xml \
-	device/ONDA/octopus-ibt/configs/sunxi-ir.kl:system/usr/keylayout/sunxi-ir.kl \
+    device/ONDA/octopus-ibt/configs/sunxi-ir.kl:system/usr/keylayout/sunxi-ir.kl \
     device/ONDA/octopus-ibt/configs/tp.idc:system/usr/idc/tp.idc \
-	device/ONDA/octopus-ibt/configs/gsensor.cfg:system/usr/gsensor.cfg \
-	device/ONDA/octopus-ibt/configs/sunxi-keyboard.kl:system/usr/keylayout/sunxi-keyboard.kl \
+    device/ONDA/octopus-ibt/configs/gsensor.cfg:system/usr/gsensor.cfg \
+    device/ONDA/octopus-ibt/configs/sunxi-keyboard.kl:system/usr/keylayout/sunxi-keyboard.kl \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
@@ -59,11 +60,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.managed_users.xml:system/etc/permissions/android.software.managed_users.xml
 
 #PRODUCT_COPY_FILES += \
-#	device/ONDA/octopus-ibt/media/bootanimation.zip:system/media/bootanimation.zip \
+#    device/ONDA/octopus-ibt/media/bootanimation.zip:system/media/bootanimation.zip \
 
 #camera config for camera detector
 PRODUCT_COPY_FILES += \
-	device/ONDA/octopus-ibt/hawkview/sensor_list_cfg.ini:system/etc/hawkview/sensor_list_cfg.ini
+    device/ONDA/octopus-ibt/hawkview/sensor_list_cfg.ini:system/etc/hawkview/sensor_list_cfg.ini
 
 # Radio Packages and Configuration Flie
 $(call inherit-product, device/softwinner/common/rild/radio_common.mk)
@@ -71,14 +72,14 @@ $(call inherit-product, device/softwinner/common/rild/radio_common.mk)
 #$(call inherit-product, device/softwinner/common/ril_modem/Oviphone/em55/oviphone_em55.mk)
 
 ## camera config for isp
-#PRODUCT_COPY_FILES += \
-#    device/softwinner/octopus-common/hawkview/8M/ov8858_4lane/isp_3a_param.ini:system/etc/hawkview/ov8858_4lane/isp_3a_param.ini \
-#    device/softwinner/octopus-common/hawkview/8M/ov8858_4lane/isp_iso_param.ini:system/etc/hawkview/ov8858_4lane/isp_iso_param.ini \
-#    device/softwinner/octopus-common/hawkview/8M/ov8858_4lane/isp_test_param.ini:system/etc/hawkview/ov8858_4lane/isp_test_param.ini \
-#    device/softwinner/octopus-common/hawkview/8M/ov8858_4lane/isp_tuning_param.ini:system/etc/hawkview/ov8858_4lane/isp_tuning_param.ini \
-#    device/softwinner/octopus-common/hawkview/8M/ov8858_4lane/bin/gamma_tbl.bin:system/etc/hawkview/ov8858_4lane/bin/gamma_tbl.bin \
-#    device/softwinner/octopus-common/hawkview/8M/ov8858_4lane/bin/hdr_tbl.bin:system/etc/hawkview/ov8858_4lane/bin/hdr_tbl.bin \
-#    device/softwinner/octopus-common/hawkview/8M/ov8858_4lane/bin/lsc_tbl.bin:system/etc/hawkview/ov8858_4lane/bin/lsc_tbl.bin
+PRODUCT_COPY_FILES += \
+    device/softwinner/octopus-common/hawkview/8M/ov8858_4lane/isp_3a_param.ini:system/etc/hawkview/ov8858_4lane/isp_3a_param.ini \
+    device/softwinner/octopus-common/hawkview/8M/ov8858_4lane/isp_iso_param.ini:system/etc/hawkview/ov8858_4lane/isp_iso_param.ini \
+    device/softwinner/octopus-common/hawkview/8M/ov8858_4lane/isp_test_param.ini:system/etc/hawkview/ov8858_4lane/isp_test_param.ini \
+    device/softwinner/octopus-common/hawkview/8M/ov8858_4lane/isp_tuning_param.ini:system/etc/hawkview/ov8858_4lane/isp_tuning_param.ini \
+    device/softwinner/octopus-common/hawkview/8M/ov8858_4lane/bin/gamma_tbl.bin:system/etc/hawkview/ov8858_4lane/bin/gamma_tbl.bin \
+    device/softwinner/octopus-common/hawkview/8M/ov8858_4lane/bin/hdr_tbl.bin:system/etc/hawkview/ov8858_4lane/bin/hdr_tbl.bin \
+    device/softwinner/octopus-common/hawkview/8M/ov8858_4lane/bin/lsc_tbl.bin:system/etc/hawkview/ov8858_4lane/bin/lsc_tbl.bin
 
 
 RODUCT_PACKAGES += Bluetooth
@@ -87,10 +88,10 @@ $(call inherit-product, hardware/realtek/bluetooth/rtl8723bs/firmware/rtlbtfw_cf
 
 #sensor    
 PRODUCT_COPY_FILES += \
-	device/ONDA/octopus-ibt/sensor.sh:system/bin/sensor.sh
+    device/ONDA/octopus-ibt/sensor.sh:system/bin/sensor.sh
   
 ## gps
-#$(call inherit-product, device/ONDA/octopus-ibt/gps/gps.mk)
+$(call inherit-product, device/ONDA/octopus-ibt/gps/gps.mk)
 
 # usb
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -99,17 +100,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.sys.mutedrm=false \
     rw.logger=0
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.product.firmware=v3.0rc1
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    ro.product.firmware=v3.0rc1
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapgrowthlimit=192m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=2m \
-    dalvik.vm.heapmaxfree=8m \
-    ro.zygote.disable_gl_preload=true
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    dalvik.vm.heapsize=512m \
+#    dalvik.vm.heapstartsize=8m \
+#    dalvik.vm.heapgrowthlimit=192m \
+#    dalvik.vm.heaptargetutilization=0.75 \
+#    dalvik.vm.heapminfree=2m \
+#    dalvik.vm.heapmaxfree=8m \
+#    ro.zygote.disable_gl_preload=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.property.tabletUI=false \
@@ -127,8 +128,8 @@ PRODUCT_CHARACTERISTICS := tablet
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.timezone=Asia/Shanghai \
     persist.sys.country=CN \
-    persist.sys.language=zh
-	  ro.sf.lcd_density=120 \
-		ro.sf.rotation=270 \
-		ro.inet.wlan.type=8723bs_vq0 \
-		ro.product.8723b_bt.used=true
+    persist.sys.language=zh \
+    ro.sf.lcd_density=120 \
+    ro.sf.rotation=270 \
+    ro.inet.wlan.type=8723bs_vq0 \
+    ro.product.8723b_bt.used=true
