641050fda9dcd07843ea1a62ef4945b82b1360e0
xunmod <116288337+xunmod@users.noreply.github.com>
2024-04-20 23:29:31 +0800

    Use pine64 source

--- build_script/build.sh
+++ build_script/build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-patch -p1 < android/device/softwinner/octopus-ibt/patches/su.patch
-cd android
-rm -r device/softwinner/common/overlay/packages/apps/Launcher2
-rm -r device/softwinner/octopus-common/overlay/packages/apps/Launcher2
+rm -r device/softwinner/common/overlay/packages/apps/Launcher2/res
+rm -r device/softwinner/octopus-common/overlay/packages
 
 sudo ln -sf /usr/bin/python2.7 /usr/bin/python
 source build/envsetup.sh
